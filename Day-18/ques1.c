542150f8712674d53cf542e62004f386fa10e844
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-02-17 17:35:16 +0530

    Day 18: Rotate Array by K Positions + Product of Array Except Self

---
+++ Day-18/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 18  |  Question 1
+ * ============================================================
+ * Title  : Rotate Array by K Positions
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Given an array of integers, rotate the array to the right by k positions.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-18/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 18  |  Question 2
+ * ============================================================
+ * Title      : Product of Array Except Self
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/product-of-array-except-self/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

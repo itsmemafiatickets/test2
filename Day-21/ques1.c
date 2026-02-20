fa7f85c2261a5b91b68855a4be8636048bca42ac
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-02-20 10:32:20 +0530

    Day 21: Create and Traverse Singly Linked List + Middle of the Linked List

---
+++ Day-21/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 21  |  Question 1
+ * ============================================================
+ * Title  : Create and Traverse Singly Linked List
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Create a singly linked list and traverse it, printing all elements.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-21/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 21  |  Question 2
+ * ============================================================
+ * Title      : Middle of the Linked List
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/middle-of-the-linked-list/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

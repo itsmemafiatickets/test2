dba1a987e5040e52495eb745cf8912af4adcbacf
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-02-21 13:15:33 +0530

    Day 22: Count Nodes in Linked List + Reverse Linked List

---
+++ Day-22/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 22  |  Question 1
+ * ============================================================
+ * Title  : Count Nodes in Linked List
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Count the total number of nodes in a singly linked list.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-22/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 22  |  Question 2
+ * ============================================================
+ * Title      : Reverse Linked List
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/reverse-linked-list/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

c7a2c1701bcd7e2298a4a049daa51814c42a60a5
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-02-23 16:49:57 +0530

    Day 24: Delete First Occurrence of a Key + Remove Linked List Elements

---
+++ Day-24/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 24  |  Question 1
+ * ============================================================
+ * Title  : Delete First Occurrence of a Key
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Delete the first occurrence of a given key from a linked list.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-24/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 24  |  Question 2
+ * ============================================================
+ * Title      : Remove Linked List Elements
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/remove-linked-list-elements/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

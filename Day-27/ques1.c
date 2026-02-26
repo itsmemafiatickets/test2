f8c694861a8faee905756299dee9671c8a8b6509
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-02-26 22:26:45 +0530

    Day 27: Find Intersection Point of Two Linked Lists + Remove Cycle in Linked List

---
+++ Day-27/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 27  |  Question 1
+ * ============================================================
+ * Title  : Find Intersection Point of Two Linked Lists
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Find the node at which two linked lists intersect.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-27/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 27  |  Question 2
+ * ============================================================
+ * Title      : Remove Cycle in Linked List
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://www.geeksforgeeks.org/problems/remove-loop-in-linked-list/1
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

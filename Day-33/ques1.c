26b36d3b22f29990f9f6bffe63c52c4bcd81cdfc
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-04 16:53:49 +0530

    Day 33: Infix to Postfix Conversion + Evaluate Reverse Polish Notation

---
+++ Day-33/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 33  |  Question 1
+ * ============================================================
+ * Title  : Infix to Postfix Conversion
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Convert an infix expression to postfix notation using a stack.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-33/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 33  |  Question 2
+ * ============================================================
+ * Title      : Evaluate Reverse Polish Notation
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/evaluate-reverse-polish-notation/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

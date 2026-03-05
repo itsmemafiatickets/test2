795456bfd0fe0ba5ac44c5863a3772c5a82414e3
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-05 16:00:03 +0530

    Day 34: Evaluate Postfix Expression + Basic Calculator II

---
+++ Day-34/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 34  |  Question 1
+ * ============================================================
+ * Title  : Evaluate Postfix Expression
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Evaluate a postfix (Reverse Polish Notation) expression.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-34/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 34  |  Question 2
+ * ============================================================
+ * Title      : Basic Calculator II
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/basic-calculator-ii/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

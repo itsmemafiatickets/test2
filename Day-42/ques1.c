4360b89081998eced00193d6e489edc07350301b
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-13 10:53:35 +0530

    Day 42: Reverse a Queue Using Stack + Find Median from Data Stream

---
+++ Day-42/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 42  |  Question 1
+ * ============================================================
+ * Title  : Reverse a Queue Using Stack
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Reverse a queue using a stack as auxiliary storage.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-42/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 42  |  Question 2
+ * ============================================================
+ * Title      : Find Median from Data Stream
+ * Platform   : LeetCode  (Hard)
+ * Link       : https://leetcode.com/problems/find-median-from-data-stream/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

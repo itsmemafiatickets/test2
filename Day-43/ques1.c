2bb11d16326031a60681e9a095c5f79403427cb9
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-14 10:46:25 +0530

    Day 43: Binary Tree Construction (Level Order) + Binary Tree Inorder Traversal

---
+++ Day-43/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 43  |  Question 1
+ * ============================================================
+ * Title  : Binary Tree Construction (Level Order)
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Construct a binary tree from level-order input.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-43/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 43  |  Question 2
+ * ============================================================
+ * Title      : Binary Tree Inorder Traversal
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/binary-tree-inorder-traversal/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

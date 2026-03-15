a20e35198e6f555df215987adb6f45ae8199a0bf
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-15 17:47:30 +0530

    Day 44: Binary Tree Traversals + Binary Tree Preorder Traversal

---
+++ Day-44/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 44  |  Question 1
+ * ============================================================
+ * Title  : Binary Tree Traversals
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Implement inorder, preorder, and postorder traversals of a binary tree.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-44/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 44  |  Question 2
+ * ============================================================
+ * Title      : Binary Tree Preorder Traversal
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

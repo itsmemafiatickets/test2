124ca7cd54f02c175acf2ef44bf40ecc6381f038
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-17 14:22:21 +0530

    Day 46: Level Order Traversal + Binary Tree Level Order Traversal

---
+++ Day-46/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 46  |  Question 1
+ * ============================================================
+ * Title  : Level Order Traversal
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Print nodes of a binary tree level by level.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-46/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 46  |  Question 2
+ * ============================================================
+ * Title      : Binary Tree Level Order Traversal
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

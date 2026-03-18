6ffe573b28d0399917d1b0d80875b2a668fe0c50
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-18 12:02:10 +0530

    Day 47: Height of Binary Tree + Maximum Depth of Binary Tree

---
+++ Day-47/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 47  |  Question 1
+ * ============================================================
+ * Title  : Height of Binary Tree
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Find the height of a binary tree using recursion.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-47/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 47  |  Question 2
+ * ============================================================
+ * Title      : Maximum Depth of Binary Tree
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

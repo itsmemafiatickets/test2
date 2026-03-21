ee4ab6fc8079faa288511f6abbaa63eebb8489e8
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-21 13:30:57 +0530

    Day 50: BST Search + Search in a Binary Search Tree

---
+++ Day-50/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 50  |  Question 1
+ * ============================================================
+ * Title  : BST Search
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Search for a key in a Binary Search Tree (BST).
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-50/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 50  |  Question 2
+ * ============================================================
+ * Title      : Search in a Binary Search Tree
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/search-in-a-binary-search-tree/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

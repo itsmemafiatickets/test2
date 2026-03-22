f1dc7a730c70134b2afe545231b94494b2fa4812
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-22 15:24:11 +0530

    Day 51: Lowest Common Ancestor in BST + Lowest Common Ancestor of a BST

---
+++ Day-51/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 51  |  Question 1
+ * ============================================================
+ * Title  : Lowest Common Ancestor in BST
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Find the Lowest Common Ancestor of two nodes in a BST.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-51/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 51  |  Question 2
+ * ============================================================
+ * Title      : Lowest Common Ancestor of a BST
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

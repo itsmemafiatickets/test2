2347fb411aea0ea349ec060c7114ec1e254b2d4b
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-23 17:46:39 +0530

    Day 52: LCA in Binary Tree + Lowest Common Ancestor of a Binary Tree

---
+++ Day-52/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 52  |  Question 1
+ * ============================================================
+ * Title  : LCA in Binary Tree
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Find the Lowest Common Ancestor of two nodes in a binary tree.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-52/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 52  |  Question 2
+ * ============================================================
+ * Title      : Lowest Common Ancestor of a Binary Tree
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

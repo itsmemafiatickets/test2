ba20f7cbd71ba89aafb21005e054dc21c8545e2e
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-24 17:17:12 +0530

    Day 53: Print Binary Tree Vertical Order + Binary Tree Level Order Traversal

---
+++ Day-53/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 53  |  Question 1
+ * ============================================================
+ * Title  : Print Binary Tree Vertical Order
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Print nodes of a binary tree in vertical order.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

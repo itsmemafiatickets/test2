c5ca5e2643d1e7cc8dcacbcd56a7a9f6b7b663aa
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-03-30 11:43:53 +0530

    Day 59: Build Tree from Inorder and Postorder + Construct Binary Tree from Inorder and Postorder Traversal

---
+++ Day-59/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 59  |  Question 1
+ * ============================================================
+ * Title  : Build Tree from Inorder and Postorder
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Build a binary tree given its inorder and postorder traversal arrays.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-59/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 59  |  Question 2
+ * ============================================================
+ * Title      : Construct Binary Tree from Inorder and Postorder Traversal
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

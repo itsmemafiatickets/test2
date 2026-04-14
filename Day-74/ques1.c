698049c7bc67d9e6db20b3be34dba90d56a2076b
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-04-14 13:26:07 +0530

    Day 74: Winner of an Election + Number of Connected Components in an Undirected Graph

---
+++ Day-74/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 74  |  Question 1
+ * ============================================================
+ * Title  : Winner of an Election
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Given votes cast for candidates, find the winner of an election.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-74/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 74  |  Question 2
+ * ============================================================
+ * Title      : Number of Connected Components in an Undirected Graph
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/number-of-provinces/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

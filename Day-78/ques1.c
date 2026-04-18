c2318e774327c80f764a1bead1f9ac07edd5d002
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-04-18 20:17:24 +0530

    Day 78: Minimum Spanning Tree using Prim's Algorithm + Articulation Points

---
+++ Day-78/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 78  |  Question 1
+ * ============================================================
+ * Title  : Minimum Spanning Tree using Prim's Algorithm
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Find the Minimum Spanning Tree of a graph using Prim's algorithm.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-78/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 78  |  Question 2
+ * ============================================================
+ * Title      : Articulation Points
+ * Platform   : LeetCode  (Hard)
+ * Link       : https://practice.geeksforgeeks.org/problems/articulation-point-1/1
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

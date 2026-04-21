ba642775d0d320c883a7b6ca1ef281802924c152
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-04-21 13:11:14 +0530

    Day 81: Implement Bubble Sort + Binary Search

---
+++ Day-81/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 81  |  Question 1
+ * ============================================================
+ * Title  : Implement Bubble Sort
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Sort an array using Bubble Sort.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-81/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 81  |  Question 2
+ * ============================================================
+ * Title      : Binary Search
+ * Platform   : LeetCode  (Easy)
+ * Link       : https://leetcode.com/problems/binary-search/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

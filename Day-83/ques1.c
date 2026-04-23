4ba227d7817641f44d4f288dd24d953b67a9a899
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-04-23 12:26:28 +0530

    Day 83: Implement Selection Sort + Search in Rotated Sorted Array

---
+++ Day-83/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 83  |  Question 1
+ * ============================================================
+ * Title  : Implement Selection Sort
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Sort an array using Selection Sort.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-83/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 83  |  Question 2
+ * ============================================================
+ * Title      : Search in Rotated Sorted Array
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

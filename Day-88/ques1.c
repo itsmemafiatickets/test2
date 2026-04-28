e7e0ed403f2d95913d3d6b4358cbf6c6afda8671
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-04-28 20:43:17 +0530

    Day 88: Aggressive Cows Problem + Magnetic Force Between Two Balls

---
+++ Day-88/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 88  |  Question 1
+ * ============================================================
+ * Title  : Aggressive Cows Problem
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Place cows in stalls such that the minimum distance between any two cows is maximized.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-88/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 88  |  Question 2
+ * ============================================================
+ * Title      : Magnetic Force Between Two Balls
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/magnetic-force-between-two-balls/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}

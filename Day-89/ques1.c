fdaff9cfba883bafe70df0816e599f137b1adddf
itsmemafiatickets <nitinguptagn2006@gmail.com>
2026-04-29 17:35:38 +0530

    Day 89: Allocate Minimum Pages + Capacity To Ship Packages Within D Days

---
+++ Day-89/ques1.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 89  |  Question 1
+ * ============================================================
+ * Title  : Allocate Minimum Pages
+ * Type   : C Programming Practice
+ * Source : https://100-days-of-dsa.vercel.app/
+ *
+ * Problem:
+ *   Allocate minimum number of pages such that the maximum pages assigned to a student is minimized.
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
---
+++ Day-89/ques2.c
@@ -0,0 +1,17 @@
+/*
+ * ============================================================
+ * 100 Days of DSA  |  Day 89  |  Question 2
+ * ============================================================
+ * Title      : Capacity To Ship Packages Within D Days
+ * Platform   : LeetCode  (Medium)
+ * Link       : https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+ * Source     : https://100-days-of-dsa.vercel.app/
+ * ============================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    /* TODO: implement solution */
+    return 0;
+}
